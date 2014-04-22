ec3047a7128827d0aea966ac54c80dad3c963fca
Mayur Patel <mayurpatel141@gmail.com>
2014-04-22 12:03:03 +0530

    added CRUD operations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,8 @@
 class ProjectsController < ApplicationController
+	before_filter :find_project, :only => [:show, :edit, :update, :destroy]
 
 	def index
-
+		@projects = Project.all
 	end
 
 	def new
@@ -20,7 +21,41 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
+		#@project = Project.find(params[:id])
 	end
+
+	def edit
+		#@project = Project.find(params[:id])
+	end
+
+	def update
+		#@project = Project.find(params[:id])
+		if @project.update_attributes(params[:project])
+			flash[:notice]="updated successfully"
+			redirect_to @project
+		else
+			flash[:notice] = "project not been edited"
+			render :action => "edit"
+		end
+	end
+
+	def destroy
+		#@project = Project.find(params[:id])
+		@project.destroy
+		flash[:notice] = "Project has been destroyed"
+		redirect_to projects_path
+	end
+
+	private
+
+		def find_project
+			@project = Project.find(params[:id])
+			rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The project you where looking "+
+											"for could not be found."
+		  redirect_to projects_path
+		end
+
+
 
 end
